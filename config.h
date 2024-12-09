d00d897dedc49b6ab22781b8f1b915f145c307be
MysteryAssss <88171916+MysteryAssss@users.noreply.github.com>
2024-12-10 04:28:12 +0700

    Add files via upload

---
+++ config.h
@@ -0,0 +1,23 @@
+// Copyright 2023 weimao (@luantty2)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define USB_DRIVER USBD2
+
+// #define SPI_SCK_PIN C10
+// #define SPI_SCK_PAL_MODE 6
+// #define SPI_MOSI_PIN C12
+// #define SPI_MOSI_PAL_MODE 6
+// #define SPI_MISO_PIN C11
+// #define SPI_MISO_PAL_MODE 6
+// #define SPI_DRIVER SPID3
+
+// #define EXTERNAL_EEPROM_SPI_SLAVE_SELECT_PIN B3
+// #define EXTERNAL_EEPROM_SPI_CLOCK_DIVISOR 16
+// #define EXTERNAL_EEPROM_BYTE_COUNT 2048
+// #define EXTERNAL_EEPROM_PAGE_SIZE 16
+
+// #define USB_MUX_SEL_PIN B14
+
+#define DEBUG_MATRIX_SCAN_RATE
---
+++ halconf.h
@@ -0,0 +1,8 @@
+// Copyright 2023 weimao (@luantty2)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// #define SPI_SELECT_MODE SPI_SELECT_MODE_PAD
+
+#include_next <halconf.h>
